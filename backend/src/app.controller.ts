87c64298f593cb6815a766ed2c7cab5bf97da965
Adnan <dev.adnan@hotmail.com>
2026-03-02 08:29:09 +0400

    chore(backend): added endpoint level logging

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,18 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { InputDto } from './dtos/input.dto';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name, { timestamp: true });
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    this.logger.log('GET /');
     return this.appService.getHello();
   }
 
   @Post('calculate')
   calculate(@Body() inputDto: InputDto) {
+    this.logger.log('POST /calculate');
     return this.appService.calculate(inputDto);
   }
 }
